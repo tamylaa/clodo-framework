771a336f678c636221e73dd2a16efc92dca94345
tamylaa <tamylatrading@gmail.com>
2025-10-24 16:13:38 +0400

    fix: import missing dependencies in generators utils index
    - Add proper imports for TemplateEngine, FileWriter, and PathResolver
    - Fix ESLint errors for undefined variables in index.js

--- src/service-management/generators/utils/index.js
+++ src/service-management/generators/utils/index.js
@@ -1,9 +1,13 @@
 /**
  * Generator Utilities - Centralized exports
- * 
+ *
  * Provides template engine, file writer, and path resolver utilities
  * for use by all generators.
  */
+
+import { TemplateEngine } from './TemplateEngine.js';
+import { FileWriter } from './FileWriter.js';
+import { PathResolver } from './PathResolver.js';
 
 export { TemplateEngine } from './TemplateEngine.js';
 export { FileWriter } from './FileWriter.js';
